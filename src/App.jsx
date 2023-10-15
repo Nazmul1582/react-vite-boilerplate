ccd921bd4c2c62142636852cf80d84ebd4412e0c
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-15 07:04:38 +0600

    routes setup with a header and some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Header />
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
   }
 ])
 
