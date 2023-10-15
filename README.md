d7cc6801684117a79d9b4f7fcdfda376202708db
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-15 07:37:19 +0600

    react-vite-tailwindcss-with-daisyui basic boilerplate

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# React-Vite-Tailwindcss Boilerplate
+# React-Vite-Tailwindcss-daisyUI Boilerplate
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, following extra plugins are available:
 
 - `tailwindcss` `postcss` `autoprefixer`
+- `daisyui`
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-center bg-teal-200 p-5 font-bold text-5xl'>React-Vite-Tailwindcss</h1>
+      <h1 className="text-center bg-teal-200 p-5 font-bold text-5xl">
+        React-Vite-Tailwindcss
+      </h1>
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="my-3 font-bold text-2xl">Daisy UI buttons</h2>
+        <div>
+          <button className="btn btn-nutral">Button</button>
+          <button className="btn btn-neutral">Neutral</button>
+          <button className="btn btn-primary">Primary</button>
+          <button className="btn btn-secondary">Secondary</button>
+          <button className="btn btn-info">Info</button>
+          <button className="btn btn-warning">Warning</button>
+          <button className="btn btn-ghost">Ghost</button>
+          <button className="btn btn-outline btn-secondary">Secondary</button>
+          <button className="btn btn-outline btn-info">Info</button>
+          <button className="btn btn-outline btn-warning">Warning</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
